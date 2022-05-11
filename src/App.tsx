1ac49c6e5dab993ba740d5bcfbc8de28a49ce129
kkuyu <devils67@naver.com>
2022-05-11 12:23:14 +0900

    Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Motion00 from "./component/Motion00";
 import Motion01 from "./component/Motion01_variants";
 import Motion02 from "./component/Motion02";
+import Motion03 from "./component/Motion03";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -18,25 +19,36 @@ const Nav = styled.nav`
   top: 0;
   right: 0;
   padding: 10px;
-  color: #fff;
   background: rgba(0, 0, 0, 0.5);
+  button {
+    color: #fff;
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
 `;
 
 function App() {
   const [number, setNumber] = useState(0);
-  const motions = [Motion00, Motion01, Motion02];
+  const motions = [
+    { name: "Basic", component: Motion00 },
+    { name: "Animation", component: Motion01 },
+    { name: "Variants", component: Motion02 },
+    { name: "Gestures", component: Motion03 },
+  ];
 
   return (
     <>
       <Nav>
         {motions.map((motion, index) => (
           <button key={index} onClick={() => setNumber(index)}>
-            {index}
+            {index === number && <span>✔</span>}
+            {motions[index].name}
           </button>
         ))}
       </Nav>
       {motions.map((motion, index) => {
-        return index === number && <Wrapper key={index}>{motions[index]()}</Wrapper>;
+        return index === number && <Wrapper key={index}>{motions[index].component()}</Wrapper>;
       })}
     </>
   );
