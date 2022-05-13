ba0d5a3d5f948f75d8d5a4690a8030830463bc5c
kkuyu <devils67@naver.com>
2022-05-13 12:41:18 +0900

    List layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Motion06 from "./component/Motion06";
 import Motion07 from "./component/Motion07";
 import Motion08 from "./component/Motion08";
 import Motion09 from "./component/Motion09";
+import Motion10 from "./component/Motion10";
 
 const Wrapper = styled.div<{ wrapperStyle?: { [key: string]: any } }>`
   height: 100vh;
@@ -48,6 +49,7 @@ function App() {
     { name: "Animations", component: Motion07 },
     { name: "Slider", component: Motion08 },
     { name: "Move", component: Motion09, wrapperStyle: { justifyContent: "space-around" } },
+    { name: "List", component: Motion10 },
   ];
 
   return (
